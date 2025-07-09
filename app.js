91040827983462bc86460f747b227ef1940f8675
Tizziano Perea <tizziano.perea@eurektrip.co>
2025-07-09 03:30:48 -0500

    First Pipeline Deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Mundo, Aplicación de Tizziano Perea!'));
+app.get('/', (req, res) => res.send('Hola Mundo, Tizziano Perea es el Mejor, Desplego usando Beanstalk, CodeBuild y CodePipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
