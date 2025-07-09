89cc01411427a759593fcab2de510c67fd0df1bc
Tizziano Perea <tizziano.perea@eurektrip.co>
2025-07-09 00:47:11 -0500

    Prueba de cambio de codigo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hola Mundo, Aplicación de Tizziano Perea!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
